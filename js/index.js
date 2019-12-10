6bd8c772122812fa02aab91e2694ea78fe73953e
unknown <cromer.dalton@gmail.com>
2019-12-10 16:54:59 -0500

    project completed

--- js/index.js
+++ js/index.js
@@ -33,6 +33,43 @@ busImg.addEventListener('mouseleave', (event) => {
      funBus.style.transition = 'all 2s';
  })
 
+ window.addEventListener('scroll', (e) => {
+     funBus.setAttribute('style', 'background-color: white;')
+     funBus.style.transform = 'scale(1.0)';
+     funBus.style.transition = 'all 2s';
+ })
+
+ window.addEventListener('resize', (e) => {
+     funBus.setAttribute('style', 'background-color: purple; color: white; transform: scale(2.0); transition: all 3s;')
+ })
+
+ const signMeUp = document.querySelectorAll('.btn');
+ const destinations = document.querySelectorAll('.destination');
+ signMeUp[0].addEventListener('click', (e) => {
+    signMeUp[0].setAttribute('style', 'background-color: #FFEBCD;')
+    destinations[0].setAttribute('style', 'transform: scale(1.5); transition: 1.5s; background-color: #FFEBCD;')
+ });
+ signMeUp[0].addEventListener('dblclick', (e) => {
+    signMeUp[0].setAttribute('style', 'background-color: #17A2B8;')
+    destinations[0].setAttribute('style', 'transform: scale(1); transition: 1.5s; background-color: white;')
+ });
+ signMeUp[1].addEventListener('click', (e) => {
+    signMeUp[1].setAttribute('style', 'background-color: #FFEBCD;')
+    destinations[1].setAttribute('style', 'transform: scale(1.5); transition: 1.5s; background-color: #FFEBCD;')
+ });
+ signMeUp[1].addEventListener('dblclick', (e) => {
+    signMeUp[1].setAttribute('style', 'background-color: #17A2B8;')
+    destinations[1].setAttribute('style', 'transform: scale(1); transition: 1.5s; background-color: white;')
+ });
+ signMeUp[2].addEventListener('click', (e) => {
+    signMeUp[2].setAttribute('style', 'background-color: #FFEBCD;')
+    destinations[2].setAttribute('style', 'transform: scale(1.5); transition: 1.5s; background-color: #FFEBCD;')
+ });
+ signMeUp[2].addEventListener('dblclick', (e) => {
+    signMeUp[2].setAttribute('style', 'background-color: #17A2B8;')
+    destinations[2].setAttribute('style', 'transform: scale(1); transition: 1.5s; background-color: white;')
+ });
+
  const stopTheLink = document.querySelectorAll('.nav-link')
 
  stopTheLink[0].addEventListener('click', (e) => {
